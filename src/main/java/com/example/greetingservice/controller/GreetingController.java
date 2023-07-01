63ddec40560d8a618b9cc7f5d8f69b7e3b6190d8
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-30 21:25:01 -0300

    acturator basic config

--- src/main/java/com/example/greetingservice/controller/GreetingController.java
+++ src/main/java/com/example/greetingservice/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.example.greetingservice.controller;
 
+import com.example.greetingservice.configuration.GreetingConfiguration;
 import com.example.greetingservice.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +12,16 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-  private static final String template = "Hello, %s!";
+  private static final String template = "%s, %s!";
   private final AtomicLong counter = new AtomicLong();
 
+  @Autowired
+  private GreetingConfiguration configuration;
+
   @RequestMapping("/greeting")
-  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-    return new Greeting(counter.incrementAndGet(), String.format(template, name));
+  public Greeting greeting(@RequestParam(value="name", defaultValue="") String name) {
+    if (name.isEmpty()) name = configuration.getDefaultValue();
+
+    return new Greeting(counter.incrementAndGet(), String.format(template, configuration.getGreeting(), name));
   }
 }
