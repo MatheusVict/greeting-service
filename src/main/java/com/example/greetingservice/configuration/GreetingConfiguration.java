a786e9e3562c4d22f33e8381fd7583e698d2c045
MatheusVict <matheusvictorhenrique@gmail.com>
2023-07-02 20:42:52 -0300

    changing serve configuration without restart the application

--- src/main/java/com/example/greetingservice/configuration/GreetingConfiguration.java
+++ src/main/java/com/example/greetingservice/configuration/GreetingConfiguration.java
@@ -1,9 +1,15 @@
 package com.example.greetingservice.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+/*
+This annotation is used to refresh the configuration properties without restarting the application,
+but you need to use the actuator endpoint /actuator/refresh
+*/
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
   private String greeting;
